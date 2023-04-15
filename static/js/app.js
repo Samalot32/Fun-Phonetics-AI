fca6407203b926ad5b194b0e599c74000b131789
elizabethtran135 <48190757+elizabethtran135@users.noreply.github.com>
2023-04-14 22:20:25 -0400

    Updated button so it changes from Try it to Done

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,61 @@
 const recordButton = document.getElementById('recordButton');
 const stopButton = document.getElementById('stopButton');
+const playButton = document.getElementById('playButton');
 const result = document.getElementById('result');
 
 let recorder;
+let isRecording = false;
 
 recordButton.addEventListener('click', async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  recorder = new RecordRTC(stream, { type: 'audio', mimeType: 'audio/webm' });
-  recorder.startRecording();
-  recordButton.disabled = true;
-  stopButton.disabled = false;
+  if (!isRecording) {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    recorder = new RecordRTC(stream, { type: 'audio', mimeType: 'audio/webm' });
+    recorder.startRecording();
+    recordButton.textContent = 'Done';
+    stopButton.disabled = false;
+    isRecording = true;
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Play it back';
+    playButton.disabled = false;
+  }
 });
 
 stopButton.addEventListener('click', async () => {
-  stopButton.disabled = true;
-  recordButton.disabled = false;
-  recorder.stopRecording(async () => {
-    const blob = await recorder.getBlob();
-    const formData = new FormData();
-    formData.append('audio', blob);
-    const response = await fetch('/recognize', { method: 'POST', body: formData });
-    const data = await response.json();
-    result.textContent = `Recognized letters: ${data.letters}`;
-  });
+  stopRecording();
+  recordButton.textContent = 'Play it back';
+  playButton.disabled = false;
 });
+
+playButton.addEventListener('click', () => {
+  playRecordedAudio();
+});
+
+async function stopRecording() {
+  if (isRecording) {
+    isRecording = false;
+    stopButton.disabled = true;
+    recordButton.textContent = 'Play it back';
+    recorder.stopRecording(async () => {
+      const blob = await recorder.getBlob();
+      recordedAudioBlob = blob;
+      playButton.disabled = false;
+      const formData = new FormData();
+      formData.append('audio', blob);
+      const response = await fetch('/recognize', { method: 'POST', body: formData });
+      const data = await response.json();
+      result.textContent = `Recognized letters: ${data.letters}`;
+    });
+  }
+}
+
+let recordedAudioBlob = null;
+
+function playRecordedAudio() {
+  if (recordedAudioBlob) {
+    const audio = new Audio(URL.createObjectURL(recordedAudioBlob));
+    audio.play();
+  } else {
+    alert('No recorded audio available.');
+  }
+}
